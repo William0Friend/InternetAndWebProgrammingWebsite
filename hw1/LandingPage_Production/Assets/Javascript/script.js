a10c3e1c0573b52fd6c9c2de311856a10437077e
William R Friend <william.friend777@gmail.com>
2023-02-06 08:08:24 -0500

    needs testimonials

--- hw1/LandingPage_Production/Assets/Javascript/script.js
+++ hw1/LandingPage_Production/Assets/Javascript/script.js
@@ -1,1 +1,15 @@
-﻿
+﻿let banner = document.getElementById("skeleton-banner");
+//banner.style.alignContent("center");
+//banner.style.maxWidth("100");
+//banner.style.height("auto");
+let win = window.onbeforeunload
+
+window.onbeforeunload = ExitPage;
+    function ExitPage()
+{
+    getit()
+    return alert("you dont exit skeltons.com...Do you?"); 
+}
+function getit() {
+console.log("I am in getit")
+}
