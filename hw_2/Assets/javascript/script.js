51e7b765e97609f7ed97cf16ca01a0e95f8ebf9a
William R Friend <william.friend777@gmail.com>
2023-02-20 16:37:34 -0500

    hw_2 needs content

---
+++ hw_2/Assets/javascript/script.js
@@ -0,0 +1,20 @@
+﻿/* play video half as fast */
+//document.querySelector('video').defaultPlaybackRate = 0.5;
+//document.querySelector('video').play();
+document.addEventListener("click", greatChoice);
+
+function greatChoice() {
+    document.getElementById("buy1").innerHTML = "Great Choice!";
+}
+
+document.addEventListener("click", () => {
+    document.getElementById("sell1").innerHTML = "CLick Me!";
+    document.getElementById("sell1").classList.add("danger");
+});
+
+
+document.addEventListener("click", () => {
+    document.getElementById("sell1").innerHTML = "CLick Me!";
+});
+
+d = document.getElementById("")
