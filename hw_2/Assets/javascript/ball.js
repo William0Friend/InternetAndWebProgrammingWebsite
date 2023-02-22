b5504ae6e6b347122a3df92bd221bb6b7abad3c2
William R Friend <william.friend777@gmail.com>
2023-02-21 23:11:39 -0500

    almost there

---
+++ hw_2/Assets/javascript/ball.js
@@ -0,0 +1,22 @@
+﻿var ball = document.getElementById('animating-ball');
+var STYLES = {
+    SCALE_UP: 'ball--scale-up',
+    SCALE_DOWN: 'ball--scale-down',
+    CIRCLING: 'ball--circling',
+};
+
+ball.addEventListener('animationend', function () {
+    if (ball.classList.contains(STYLES.SCALE_DOWN)) {
+        ball.classList.remove(STYLES.SCALE_DOWN)
+        ball.classList.remove(STYLES.SCALE_UP)
+        ball.classList.remove(STYLES.CIRCLING);
+    } else if (ball.classList.contains(STYLES.CIRCLING)) {
+        ball.classList.add(STYLES.SCALE_DOWN);
+    } else if (ball.classList.contains(STYLES.SCALE_UP)) {
+        ball.classList.add(STYLES.CIRCLING);
+    }
+});
+
+ball.onclick = function () {
+    ball.classList.add(STYLES.SCALE_UP);
+}
--- hw_2/Assets/javascript/progressbarJquery.js
+++ hw_2/Assets/javascript/progressbarJquery.js
@@ -49,25 +49,3 @@ $(document).ready(function () {
 
 
 
-
-
-
-
-//Change button dynmamically, does not work right, needs events seperated to variables
-document.addEventListener("click", greatChoice);
-
-function greatChoice() {
-    document.getElementById("buy1").innerHTML = "Great Choice!";
-}
-
-document.addEventListener("click", () => {
-    document.getElementById("sell1").innerHTML = "CLick Me!";
-    document.getElementById("sell1").classList.add("danger");
-});
-
-
-document.addEventListener("click", () => {
-    document.getElementById("sell1").innerHTML = "CLick Me!";
-});
-
-d = document.getElementById("")
