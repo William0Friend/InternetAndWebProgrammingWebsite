9ba2caf4d4e93d53a4a991ec9abd50812fb014be
William R Friend <william.friend777@gmail.com>
2023-02-24 18:39:16 -0500

    added javascript libs

---
+++ hw2/Assets/javascript/events.js
@@ -0,0 +1,9 @@
+//STILL IN PROGRESS
+// Will hold all actions based on events
+// for event based only
+
+/* play video half as fast */
+//document.querySelector('video').defaultPlaybackRate = 0.5;
+//document.querySelector('video').play();
+
+//wiggle = document.getElementById("btn-black-square-1");
---
+++ hw3/Assets/javascript/test.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let sum = 0, leftsum = 0;
+    //nums.forEach(loop);
+   //function loop(item){
+   // sum += item
+//}
+    for (let j = 0; j < nums.length ; j++ ){
+        sum += nums[j];
+    }
+    for (let i = 0; i < nums.length; i++){
+        if(leftsum === (sum - leftsum - nums[i]))
+            return i;
+        leftsum += nums[i]
+    }
+    return -1;
+};
+
+answer = pivotIndex([1,7,3,6,5,6])
+console.log(answer)
+
+answerSection = document.createElement("section");
+answerDiv = answerSection.createElement("div");
+answerObject = answerDiv.createElement("object");
+answerSection
