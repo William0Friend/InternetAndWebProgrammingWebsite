8e2642679c61c95031993bd5f348c37da7e40aa8
William R Friend <william.friend777@gmail.com>
2023-03-12 15:13:45 -0400

    midterm_production needs finishes

---
+++ Midterm-Prog-Part-2023_production/Midterm-Prog-Part-2023/ReadMe.txt
@@ -0,0 +1,11 @@
+﻿TODO:
+
+DONE:
+I used JQuery to read in an XML file (stocks.xml) which contains information about stocks. The XML is placed in an XML document object.
+Display the list of stock ticker symbols.  When displayed, sort the stocks alphabetical order by ticker symbol.  Note that these stocks are not sorted in the XML file.  
+Sorting should be done sorting routine in JS/JQ.  IMPORTANT:  Do not create a sorted list simply by rearranging the <Stock> elements in the XML file (as I will check for this).  
+When the mouse hovers over one of the stocks in the list, the HTML for the additional information about the stock is generated and placed in the <div id=”stockInfo”> element (see Figure 2).  
+Note that this part involves accessing the XML through the DOM to get the additional information.  In addition, the color of the selected item will change from navy to red.
+When the mouse no longer hovers over the item, the color of the text will change back to navy. 
+Between 3-5 seconds must pass, during which time the information will linger.  After that, the <div id=”stockInfo”> element must clear.  
+
---
+++ Midterm-Prog-Part-2023_production/Midterm-Prog-Part-2023/js/stocksJQuery.js
@@ -0,0 +1,95 @@
+﻿// Wait for the document to be ready before running the script
+$(document).ready(function () {
+
+    // Use AJAX to get the XML file containing the stocks data
+    $.ajax({
+        type: "GET",
+        url: "stocks.xml",
+        dataType: "xml",
+        success: function (xml) {
+
+            // Find all the Stock elements in the XML file and sort them by symbol
+            var $stocks = $(xml).find("Stock");
+            $stocks.sort(function (a, b) {
+                return $(a).attr("symbol").localeCompare($(b).attr("symbol"));
+            });
+
+            // Find the stock list element in the HTML
+            var $stockList = $("#stockList");
+
+            // Loop through each stock in the XML and create a link for it in the stock list
+            $stocks.each(function () {
+                var $stock = $(this);
+                var symbol = $stock.attr("symbol");
+
+                // Create a link with the stock symbol as the text
+                var $symbolLink = $("<li>")
+                    .text(symbol)
+                    .css("color", "navy")
+                    // When the user hovers over the link, show the stock information
+                    .mouseenter(function () {
+                        $(this).css("color", "red");
+
+                        // Find the stock info element in the HTML
+                        var $stockInfo = $("#stockInfo");
+
+                        // Wait 1 seconds before showing the stock information
+                        setTimeout(function () {
+                            // Clear any previous stock information
+                            $stockInfo.empty();
+
+                            // Find the stock's company, market, sector, price, year range, and dividend information in the XML
+                            var $company = $stock.find("Company");
+                            var $market = $stock.find("Market");
+                            var $sector = $stock.find("Sector");
+                            var $price = $stock.find("Price");
+                            var $low = $stock.find("YearRange Low");
+                            var $high = $stock.find("YearRange High");
+                            var $dividend = $stock.find("Dividend");
+
+                            // Create a string with all the stock information
+                            //var html = "<strong>" + symbol + "</strong><br>" + "<ol>" +
+                            var html = "<ol>" +
+
+                                "<li>Company = " + $company.text() + "</li><br>" +
+                                "<li>Market = " + $market.text() + "</li><br>" +
+                                "<li>Sector = " + $sector.text() + "</li><br>" +
+                                "<li>Price = " + $price.text() + "</li><br>" +
+                                "<li>Year Range = " + $low.text() + " - " + $high.text() + "<br>";
+
+                            // If the stock has a dividend, add the dividend information to the string
+                            if ($dividend.attr("available") == "true") {
+                                var $amount = $dividend.find("Amount");
+                                var $yield = $dividend.find("Yield");
+                                var $frequency = $dividend.find("Frequency");
+
+                                html += "<br>" +
+                                    "<li>Dividend = " + $amount.text() + " (" + $yield.text() + " yield, " + $frequency.text() + " frequency)" + "</li><br></ol>";
+                            }
+                            // If the stock does not have a dividend, add a message to the string
+                            if ($dividend.attr("available") == "false") {
+                                html += "<br>" +
+                                    "<li> Dividend is not available </li><br> </ol>";
+                            }
+                            // Add the stock information to the stock info element
+                            $stockInfo.html(html);
+                        }, 500);//1 second load
+                    })
+                   
+                    // When the user stops hovering over the link, change the color back to navy
+                    // and remove the stock information after 5 seconds
+                    .mouseleave(function () {
+                        $(this).css("color", "navy");
+                        setTimeout(function () {
+                            $("#stockInfo").empty();
+                        }, 5000); // 5 seconds
+                    });
+                // Append the stock symbol link to the stock list
+                $stockList.append($symbolLink);
+
+                // Append a line break after the stock symbol link
+                $stockList.append("<br>");
+            });
+        }
+    });
+});
