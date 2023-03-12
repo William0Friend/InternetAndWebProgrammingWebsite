3d97ea048203b533a9336213e05339ff3737edc5
William R Friend <william.friend777@gmail.com>
2023-03-12 14:28:51 -0400

    midterm done

---
+++ Midterm-Prog-Part-2023/Midterm-Prog-Part-2023/js/jq.js
@@ -0,0 +1,70 @@
+﻿< !--< script >
+	$(document).ready(function () {
+		$.ajax({
+			type: "GET",
+			url: "stocks.xml",
+			dataType: "xml",
+			success: function (xml) {
+				var $stocks = $(xml).find("Stock");
+
+				$stocks.sort(function (a, b) {
+					return $(a).attr("symbol").localeCompare($(b).attr("symbol"));
+				});
+
+				var $stockList = $("#stockList");
+
+				$stocks.each(function () {
+					var $stock = $(this);
+					var symbol = $stock.attr("symbol");
+					var $symbolLink = $("<a> <li>")
+						.text(symbol)
+						.css("color", "navy")
+						.mouseenter(function () {
+							$(this).css("color", "red");
+
+							var $stockInfo = $("#stockInfo");
+
+							setTimeout(function () {
+								$stockInfo.empty();
+
+								var $company = $stock.find("Company");
+								var $market = $stock.find("Market");
+								var $sector = $stock.find("Sector");
+								var $price = $stock.find("Price");
+								var $low = $stock.find("YearRange Low");
+								var $high = $stock.find("YearRange High");
+								var $dividend = $stock.find("Dividend");
+
+								var html = "<strong>" + symbol + "</strong><br>" +
+									"Company: " + $company.text() + "<br>" +
+									"Market: " + $market.text() + "<br>" +
+									"Sector: " + $sector.text() + "<br>" +
+									"Price: " + $price.text() + "<br>" +
+									"Year Range: " + $low.text() + " - " + $high.text();
+
+								if ($dividend.attr("available") == "true") {
+									var $amount = $dividend.find("Amount");
+									var $yield = $dividend.find("Yield");
+									var $frequency = $dividend.find("Frequency");
+
+									html += "<br>" +
+										"Dividend: " + $amount.text() + " (" + $yield.text() + " yield, " + $frequency.text() + " frequency)";
+								}
+								if ($dividend.attr("available") == "false") {
+									html += "<br>" +
+										"Dividend is not avalible";
+								}
+								$stockInfo.html(html);
+							}, 3000);
+						})
+						.mouseleave(function () {
+							$(this).css("color", "navy");
+						});
+
+					$stockList.append($symbolLink);
+					$stockList.append("<br>");
+				});
+			}
+		});
+	});
+	</script > -->
---
+++ Midterm-Prog-Part-2023/Midterm-Prog-Part-2023/js/src.js
@@ -0,0 +1,88 @@
+﻿
+(function loadXMLDoc(dname) {
+    let xmlDoc;
+    try {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('GET', dname, false);
+        xmlhttp.setRequestHeader('Content-Type', 'text/xml');
+        xmlhttp.send('');
+        xmlDoc = xmlhttp.responseXML;
+    }
+    catch (e) {
+        console.error(e.message);
+    }
+    return xmlDoc;
+});
+
+
+
+(function onBodyLoad() {
+    //let div2 = getElementById("div2");
+    let xmlDocument = loadXMLDoc("stocks.xml")
+
+    // get the root element
+    // Portfolio
+    let element = xmlDocument.documentElement;
+
+
+    let stockList = document.getElementById('stockList');
+
+    stockList.innerHTML = "<p>Here is the root node of the document: " +
+        "<strong>" + element.nodeName + "</strong>" +
+        "<br />The following are its child elements:" + element.childElementCount
+    "</p><ul>";
+
+    // traverse all child nodes of root element
+    let curNode = element.firstElementChild;
+    let stockNode = element.firstElementChild;
+    let curNodeArray = [];
+    ///let count = 0;
+    while (curNode != null) {
+        // print node name of each child element
+        let attribute = "symbol";
+        let value = curNode.getAttribute(attribute);
+        stockList.innerHTML += "<li><strong>" + value + "</strong></li>";
+        curNodeArray.pop(value);
+        console.log(curNodeArray);
+        curNode = curNode.nextElementSibling;
+        //count++;
+    }
+    stockList.innerHTML += "</ol>";
+});
+
+
+
+
+
+
+(function showInfo() {
+    //let div2 = getElementById("div2");
+    let xmlDocument = loadXMLDoc("stocks.xml")
+
+    // get the root element
+    // Portfolio
+    //let element = xmlDocument.documentElement;
+    let portfolioNode = xmlDocument.documentElement;
+
+    let stockInfo = document.getElementById('stockInfo');
+    //Here is the root node of the document: Portfolio
+    //The following are its child elements: 5
+    stockInfo.innerHTML = "<p>Here is the root node of the document: " +
+        "<strong>" + portfolioNode.nodeName + "</strong>" +
+        "<br />The following are its child elements:" + portfolioNode.childElementCount
+    "</p><ol>";
+
+    // get the first child node of root element
+    //let currentNode = element.firstElementChild;
+    let stockNode = portfolioNode.firstElementChild;
+    // traverse all child nodes of root element
+    // Company
+    let curNode = stockNode.firstElementChild;
+    //stockInfo.innerHTML = "<ol>";
+    while (curNode != null) {
+        // print node name of each child element
+        stockInfo.innerHTML += "<li><strong>" + curNode.nodeName + " - " + curNode.textContent + "</strong></li>";
+        curNode = curNode.nextElementSibling;
+    }
+    stockInfo.innerHTML = "/<ol>";
+});
