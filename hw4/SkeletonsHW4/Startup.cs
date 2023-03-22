95c096e25c830b25ebfd49c66387b74042e22f36
William R Friend <william.friend777@gmail.com>
2023-03-22 15:10:24 -0400

    test web app

---
+++ hw4/SkeletonsHW4/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkeletonsHW4.Startup))]
+namespace SkeletonsHW4
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
